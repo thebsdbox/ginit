56c7b5fc747433fb75734cfde152c7b1b795c975
thebsdbox <dan@thebsdbox.co.uk>
2022-03-03 13:19:21 +0000

    Fixes to ginit script

--- ginit.sh
+++ ginit.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-echo "Greetings Professor Falken. Would you like to play a game?"
-sleep 4 # For dramatic effect
-echo "Downloading you a Focal Ubuntu Kernel"
-wget http://archive.ubuntu.com/ubuntu/dists/focal-updates/main/installer-amd64/current/legacy-images/netboot/ubuntu-installer/amd64/linux
+#Lines from Tron: Legacy
+echo "$ whoami"
+echo "flynn"
+sleep 0.5
+echo "$ uname -a"
+echo "SolarOS 4.0.1 Generic_50203-02 sun4m i386"
+echo "Unknown.Unknown"
+sleep 0.5
+echo "$ login -n root"
+echo "Login incorrect"
+sleep 0.5
+echo "login:backdoor"
+echo "No home directory specified in password file!"
+echo "Logging in with home=/"
+
+echo "#"
+echo "# bin/build_world"
+sleep 3
+
+if [ -f "./linux" ]; then
+    echo "Kernel already exists."
+else 
+    echo "Downloading you a Focal Ubuntu Kernel"
+    wget http://archive.ubuntu.com/ubuntu/dists/focal-updates/main/installer-amd64/current/legacy-images/netboot/ubuntu-installer/amd64/linux
+fi
+
 echo "Creating your ramdisk"
 docker buildx build  --platform linux/amd64 --load -t ginit:0.0 -f initrd.Dockerfile .
 docker create --name exporter ginit:0.0 null
